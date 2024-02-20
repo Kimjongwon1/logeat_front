7d975e2fb028613ea172d80757711ca450a8edaa
Kimjongwon1 <amricanoblack@naver.com>
2024-02-20 12:32:45 +0900

    first

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // 라우터 임포트
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app'); // 라우터 사용
